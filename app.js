eff360a68d40770650f9f189cf9960959fb796aa
Frank <franksu49@gmail.com>
2019-12-20 19:46:37 -0500

    modified port

--- app.js
+++ app.js
@@ -73,6 +73,6 @@ app.use("/", indexRoutes);
 app.use("/ballcourts/:id/comments", commentRoutes);
 app.use("/ballcourts", ballcourtRoutes);
 
-app.listen(3000, function() {
+app.listen((process.env.PORT || 5000), function() {
     console.log("The YelpCamp Server Has Started!");
 });
